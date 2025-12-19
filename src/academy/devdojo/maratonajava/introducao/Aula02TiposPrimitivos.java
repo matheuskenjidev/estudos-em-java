800ad95f50641f354735e0d55e626ca18ea0d949
Matheus Kenji <matheuskenjidev@gmail.com>
2025-12-19 13:50:15 -0300

    tipos primitivos em java e concatenação de strings

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // tipos primitivos:Em Java, variáveis primitivas são variáveis
+        // que guardam valores simples e diretos, não objetos.
+        //Elas armazenam o valor em si, e não uma referência.
+
+
+        //int, double, float, char, byte, short, long, boolean
+
+        int notaDoPai = 36;
+        System.out.println("a idade do pai é: " + notaDoPai + " anos");
+
+    }
+}
