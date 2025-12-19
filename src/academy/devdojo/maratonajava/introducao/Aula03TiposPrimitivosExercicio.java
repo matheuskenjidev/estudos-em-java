a4bb3ccdf9481b5e70667b515a585328cec70cab
Matheus Kenji <matheuskenjidev@gmail.com>
2025-12-19 15:04:31 -0300

    string e exercício de tipos primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Matheus";
+        String endereco = "Rua dos patinhos, 29";
+        double salario = 2000.0;
+        String data = "28/12/2002";
+
+        System.out.println("eu " + nome + ", morando no endereço " + endereco + " confirmo que recebi o salário de " + salario + " na data " + data);
+    }
+}
