1311710a27ec30c6e3cfc1a2f767d100a929febc
Matheus Kenji <matheuskenjidev@gmail.com>
2025-12-19 13:36:10 -0300

    aprendendo os tipos de comentários em java e como usar

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,18 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class OlaDevDojo {
+    //comentário de uma linha
+
+    /*comentário
+        de
+        multiplas
+        linhas
+    */
+
+    /**
+     * classe que printa salve mundo <p>salve</p>
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Salve mundo");
     }
