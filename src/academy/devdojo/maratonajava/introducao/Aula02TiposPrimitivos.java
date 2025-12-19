5ebbfa72a32db0941e4eb7ada6e99bcf9bc479fc
Matheus Kenji <matheuskenjidev@gmail.com>
2025-12-19 14:46:05 -0300

    casting, string e var

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -9,8 +9,23 @@ public class Aula02TiposPrimitivos {
 
         //int, double, float, char, byte, short, long, boolean
 
+        long numeroGrande = 100000L;//casting (int) 10000000 força o long a virar int
         int notaDoPai = 36;
-        System.out.println("a idade do pai é: " + notaDoPai + " anos");
+        double salarioDouble = 2000.0;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 19;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        char caractere = 'M';
 
+        //OBS: string n é primitiva, pois é um valor por referencia
+        String nome = "Matheus Kenji";
+
+        System.out.println("a idade do pai é: " + notaDoPai + " anos");
+        System.out.println(nome);
+
+        //java 10
+        var quantidadeDeLivros = 20;
+        System.out.println(quantidadeDeLivros);
     }
 }
