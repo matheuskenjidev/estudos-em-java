0fb82e504534f58bec0eb3e79fc1bce63382073e
Matheus Kenji <matheuskenjidev@gmail.com>
2025-12-20 10:58:31 -0300

    exercício usando if, elseif e else

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais04 {
+    public static void main(String[] args) {
+        double salario = 45000.0;
+        double imposto;
+
+        if(salario <= 34712) {
+            imposto = salario * (9.70 / 100);
+        } else if (salario >= 34713 && salario <= 68587) {
+            imposto = salario * (37.35 / 100);
+        } else {
+            imposto = salario * (49.50 / 100);
+        }
+        System.out.println(imposto);
+    }
+}
