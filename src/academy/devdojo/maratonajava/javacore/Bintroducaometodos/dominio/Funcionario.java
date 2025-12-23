34df75cf800eff326b9a3563b2efa58e9d1ee55a
Matheus Kenji <matheuskenjidev@gmail.com>
2025-12-23 11:28:22 -0300

    modificador de acesso private

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,9 +1,11 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private  double[] salarios;
+    private double media;
+
 
     public void imprime() {
         System.out.println(this.nome);
@@ -23,5 +25,37 @@ public class Funcionario {
         }
 
         System.out.println("Média salarial em 3 meses é = " + somaSalario / this.salarios.length);
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public double[] getSalarios() {
+        return this.salarios;
+    }
+
+    public double getMedia() {
+        return this.media;
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -5,9 +5,6 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funciona
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.idade = 23;
-        funcionario.nome = "Matheus";
-        funcionario.salarios = new double[]{1200, 987.32, 2000};
 
         funcionario.imprime();
         funcionario.imprimeMediaSalario();
