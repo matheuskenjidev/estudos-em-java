eaaf391f53c740fb82b8d1f280762526eae67aba
Matheus Kenji <matheuskenjidev@gmail.com>
2026-01-16 12:10:24 -0300

    Tokens e Delimitadores

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/ScannerTest02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.Scanner;
+
+public class ScannerTest02 {
+    public static void main(String[] args) {
+        String texto = "Levi, Eren, Mikasa";
+        Scanner scanner = new Scanner(texto);
+        scanner.useDelimiter(",");
+        System.out.println(scanner);
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.next().trim());
+        }
+    }
+}
